1444e7995e4ff4dce3440220d73bc71a1d43b499
Lewis Gill <161453261+lg2050hw@users.noreply.github.com>
2024-03-13 15:27:41 +0000

    Update game.cpp V1.2

--- game.cpp
+++ game.cpp
@@ -1,18 +1,35 @@
-/* number guesser game */
-
-#include stdio.h
-
-void()
-{
-printf("pick a number between 1- 5 and answer the following yes or no questions");
-
-
-
-
-
-
-
-
-
-
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n,i;          
+    int number;     
+    srand(time(0));
+    number=rand()%51;
+    cout<<"Guessa number from 1-50, you have 5 attempts to guess: ";
+    for(i=0;i<5;i++)
+    {
+    cin>>n;
+    if(number==n) {
+      cout<<"\nWell Done, you guessed the correct number, "<<number;
+      cout<<"\nAttempts you have used: "<<i+1;
+      break;
+    }
+    else (number>n) {
+      cout<<"\nYou guessed less than correct number, Attempts remaining: "<<5-(i+1);
+      if(5-(i+1)>0) {
+          cout<<"\nAttempt again, choose a number more than last time: ";
+        }
+    }
+    else (number<n) {
+      cout<<"\nYou guessed more than correct number, Attempts remaining: "<<5-(i+1);
+      if(5-(i+1)>0) {
+          cout<<"\nAttempt again, choose a number more than last time: ";
+        }
+    }
+  }
+  if(i==5) {
+    cout<<"\n\nUnlucky, you ran out of guesses, better luk next time.";
+    cout<<"\nThe number I was looking for is: "<<number;
+  }
+    return 0;
 }
